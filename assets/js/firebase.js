71be323dda126faeba4052ca3f1a226f3b37ad00
Somnath Paul <somnath.20.paul@gmail.com>
2026-01-22 08:45:46 +0530

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -3,8 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  // 🔴 your real config here
-};
+    apiKey: "AIzaSyA2Fgptok0ZzqJhbNrEl0a8j_rCou9-ULk",
+    authDomain: "datanautiq.firebaseapp.com",
+    projectId: "datanautiq",
+    storageBucket: "datanautiq.firebasestorage.app",
+    messagingSenderId: "214896542682",
+    appId: "1:214896542682:web:dc54d4f52b8ad33175f6e3",
+    measurementId: "G-PRBDSYJTZS"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
